c948a4f1b43e5d6aeb371a782522688def6f3548
Etienne Baque <etiennebaque@gmail.com>
2015-08-01 08:34:48 +0600

    Added Ad model + route to check all ads

--- madloba-api/Handlers.go
+++ madloba-api/Handlers.go
@@ -6,7 +6,35 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+
+	"database/sql"
+	_ "github.com/lib/pq"
 )
+
+func AdIndex(w http.ResponseWriter, r *http.Request) {
+	db, err := sql.Open("postgres", "user=etienne dbname=madloba password=postgres sslmode=disable")
+	if err != nil {
+		panic(err)
+	}
+
+	rows, err := db.Query("select id, title, description from ads;")
+	var ads Ads
+
+	for rows.Next() {
+		var id int
+		var title string
+		var description string
+		err = rows.Scan(&id, &title, &description)
+		ad := Ad{Id: id, Title: title, Description: description}
+		ads = append(ads, ad)
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+    w.WriteHeader(http.StatusOK)
+    if err := json.NewEncoder(w).Encode(ads); err != nil {
+        panic(err)
+    }
+}
 
 func Index(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Welcome!")
